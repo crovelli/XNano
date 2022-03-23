d7318b3b5bbff5aa1ad0c891ff04c9b7b0be3b20
crovelli <chiara.rovelli@cern.ch>
2022-03-23 09:55:44 +0100

    first commit slim version

--- plugins/PiPiBuilder.cc
+++ plugins/PiPiBuilder.cc
@@ -7,20 +7,17 @@
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h"
-
-#include <vector>
-#include <memory>
-#include <map>
-#include <string>
 #include "CommonTools/Utils/interface/StringCutObjectSelector.h"
 #include "DataFormats/PatCandidates/interface/CompositeCandidate.h"
 #include "DataFormats/Math/interface/deltaR.h"
 #include "CommonTools/Statistics/interface/ChiSquaredProbability.h"
 #include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+
+#include <vector>
+#include <string>
+#include "TLorentzVector.h"
+
 #include "helper.h"
-#include <limits>
-#include <algorithm>
-#include "KinVtxFitter.h"
 
 class PiPiBuilder : public edm::global::EDProducer<> {
 
@@ -29,14 +26,11 @@ public:
   typedef std::vector<reco::TransientTrack> TransientTrackCollection;
   
   explicit PiPiBuilder(const edm::ParameterSet &cfg):
-    trk1_selection_{cfg.getParameter<std::string>("trk1Selection")},
-    trk2_selection_{cfg.getParameter<std::string>("trk2Selection")}, 
+    trk_selection_{cfg.getParameter<std::string>("trkSelection")},
     pre_vtx_selection_{cfg.getParameter<std::string>("preVtxSelection")},
-    post_vtx_selection_{cfg.getParameter<std::string>("postVtxSelection")},
-    pfcands_{consumes<pat::CompositeCandidateCollection>( cfg.getParameter<edm::InputTag>("pfcands") )},
-    ttracks_{consumes<TransientTrackCollection>( cfg.getParameter<edm::InputTag>("transientTracks") )} {
+    dimuons_{consumes<pat::CompositeCandidateCollection>( cfg.getParameter<edm::InputTag>("dimuons") )},
+    pfcands_{consumes<pat::CompositeCandidateCollection>( cfg.getParameter<edm::InputTag>("pfcands") )} {
       produces<pat::CompositeCandidateCollection>("SelectedDiPions");
-      produces<std::vector<KinVtxFitter> >("SelectedDiPionKinVtxs");  
     }
 
   ~PiPiBuilder() override {}
@@ -46,92 +40,110 @@ public:
   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions) {}
   
 private:
-  const StringCutObjectSelector<pat::CompositeCandidate> trk1_selection_;     
-  const StringCutObjectSelector<pat::CompositeCandidate> trk2_selection_;     
+  const StringCutObjectSelector<pat::CompositeCandidate> trk_selection_;     
   const StringCutObjectSelector<pat::CompositeCandidate> pre_vtx_selection_;
-  const StringCutObjectSelector<pat::CompositeCandidate> post_vtx_selection_;
+  const edm::EDGetTokenT<pat::CompositeCandidateCollection> dimuons_;
   const edm::EDGetTokenT<pat::CompositeCandidateCollection> pfcands_; 
-  const edm::EDGetTokenT<TransientTrackCollection> ttracks_; 
 };
-
 
 void PiPiBuilder::produce(edm::StreamID, edm::Event &evt, edm::EventSetup const &) const {
 
   // inputs  
+  edm::Handle<pat::CompositeCandidateCollection> dimuons;
+  evt.getByToken(dimuons_, dimuons);  
   edm::Handle<pat::CompositeCandidateCollection> pfcands;
   evt.getByToken(pfcands_, pfcands);  
-  edm::Handle<TransientTrackCollection> ttracks;
-  evt.getByToken(ttracks_, ttracks);
   
   // output
   std::unique_ptr<pat::CompositeCandidateCollection> pipi_out(new pat::CompositeCandidateCollection());
-  std::unique_ptr<std::vector<KinVtxFitter> > kinVtx_out( new std::vector<KinVtxFitter> );   
   
-  // main loop
-  for(size_t trk1_idx = 0; trk1_idx < pfcands->size(); ++trk1_idx ){
-    edm::Ptr<pat::CompositeCandidate> trk1_ptr( pfcands, trk1_idx );
-    if(!trk1_selection_(*trk1_ptr)) continue; 
+
+  // Start from muons, to reduce the combinatorics
+  for(size_t ll_idx = 0; ll_idx < dimuons->size(); ++ll_idx) {   
     
-    for(size_t trk2_idx = trk1_idx + 1; trk2_idx < pfcands->size(); ++trk2_idx) {
-      edm::Ptr<pat::CompositeCandidate> trk2_ptr( pfcands, trk2_idx );
-      if(!trk2_selection_(*trk2_ptr)) continue;
+    edm::Ptr<pat::CompositeCandidate> ll_ptr(dimuons, ll_idx);
+    
+    TLorentzVector p4mu1, p4mu2;
+    p4mu1.SetPtEtaPhiM(ll_ptr->userFloat("mu1_pt"), ll_ptr->userFloat("mu1_eta"), ll_ptr->userFloat("mu1_phi"), MUON_MASS);
+    p4mu2.SetPtEtaPhiM(ll_ptr->userFloat("mu2_pt"), ll_ptr->userFloat("mu2_eta"), ll_ptr->userFloat("mu2_phi"), MUON_MASS);
 
-      // opposite charge
-      if (trk1_ptr->charge() == trk2_ptr->charge()) continue; 
-
-      // create a pi+ / pi- pair
-      pat::CompositeCandidate pipi_cand;
-      auto trk1_p4=trk1_ptr->polarP4();
-      auto trk2_p4=trk2_ptr->polarP4();
-      trk1_p4.SetM(PI_MASS);
-      trk2_p4.SetM(PI_MASS);
-
-      // adding stuff for pre fit selection
-      pipi_cand.setP4(trk1_p4 + trk2_p4);
-      pipi_cand.addUserFloat("trk_deltaR", reco::deltaR(*trk1_ptr, *trk2_ptr));
-
-      // Save candidates
-      pipi_cand.addUserCand("trk1", trk1_ptr );
-      pipi_cand.addUserCand("trk2", trk2_ptr );
-
-      // save indices
-      pipi_cand.addUserInt("trk1_idx", trk1_idx );
-      pipi_cand.addUserInt("trk2_idx", trk2_idx );
+    // Loop over pions, I
+    for(size_t trk1_idx = 0; trk1_idx < pfcands->size(); ++trk1_idx ){
+      edm::Ptr<pat::CompositeCandidate> trk1_ptr( pfcands, trk1_idx );
+      if(!trk_selection_(*trk1_ptr)) continue; 
       
-      // selection before fit
-      if( !pre_vtx_selection_(pipi_cand) ) continue;
+      TLorentzVector trk1;
+      trk1.SetPtEtaPhiM(trk1_ptr->pt(), trk1_ptr->eta(), trk1_ptr->phi(), PI_MASS);
 
-      KinVtxFitter fitter(
-			  {ttracks->at(trk1_idx), ttracks->at(trk2_idx)},
-			  {PI_MASS, PI_MASS },
-			  {PI_SIGMA, PI_SIGMA} 
-			  );
-      if ( !fitter.success() ) continue;           
-      
-      // save quantities after fit
-      pipi_cand.addUserFloat("sv_chi2", fitter.chi2());
-      pipi_cand.addUserFloat("sv_ndof", fitter.dof()); 
-      pipi_cand.addUserFloat("sv_prob", fitter.prob());    
-      pipi_cand.addUserFloat("fitted_mass", fitter.fitted_candidate().mass());
+      // hardcoded...
+      if (trk1.DeltaR(p4mu1) < 0.01) continue;
+      if (trk1.DeltaR(p4mu2) < 0.01) continue;
+      if ((p4mu1 + p4mu2 + trk1).M() - (p4mu1 + p4mu2).M() + JPSI_MASS > 4.0) continue; 
 
-      // refitted daughters (tracks)     
-      // std::vector<std::string> dnames{ "trk1", "trk2" };
-      // for (size_t idaughter=0; idaughter<dnames.size(); idaughter++){
-      // pipi_cand.addUserFloat("fitted_" + dnames[idaughter] + "_pt" ,fitter.daughter_p4(idaughter).pt() );
-      // pipi_cand.addUserFloat("fitted_" + dnames[idaughter] + "_eta",fitter.daughter_p4(idaughter).eta() );
-      // pipi_cand.addUserFloat("fitted_" + dnames[idaughter] + "_phi",fitter.daughter_p4(idaughter).phi() );
-      // }
 
-      // after fit selection 
-      if( !post_vtx_selection_(pipi_cand) ) continue;
+      // Loop over pions, II
+      for(size_t trk2_idx = trk1_idx + 1; trk2_idx < pfcands->size(); ++trk2_idx) {
+	edm::Ptr<pat::CompositeCandidate> trk2_ptr( pfcands, trk2_idx );
+	if(!trk_selection_(*trk2_ptr)) continue;
+	
+	// opposite charge
+	if (trk1_ptr->charge() == trk2_ptr->charge()) continue; 
 
-      pipi_out->push_back(pipi_cand);
-      kinVtx_out->push_back(fitter);   
-    }
-  }
-  
+	TLorentzVector trk2;
+	trk2.SetPtEtaPhiM(trk2_ptr->pt(), trk2_ptr->eta(), trk2_ptr->phi(), PI_MASS);
+	
+	// hardcoded...
+	if (trk2.DeltaR(p4mu1) < 0.01) continue;
+	if (trk2.DeltaR(p4mu2) < 0.01) continue;
+	if ((p4mu1 + p4mu2 + trk1 + trk2).M() - (p4mu1 + p4mu2).M() + JPSI_MASS > 4.1) continue; 
+
+
+	// Now I've a pair of pions which matches the two selected muons
+	pat::CompositeCandidate pipi_cand;
+	auto trk1_p4=trk1_ptr->polarP4();
+	auto trk2_p4=trk2_ptr->polarP4();
+	trk1_p4.SetM(PI_MASS);
+	trk2_p4.SetM(PI_MASS);
+
+	// adding stuff for pre fit selection
+	pipi_cand.setP4(trk1_p4 + trk2_p4);
+	pipi_cand.addUserFloat("trk_deltaR", reco::deltaR(*trk1_ptr, *trk2_ptr));
+	
+	// Save candidates and indices
+	pipi_cand.addUserCand("trk1", trk1_ptr );
+	pipi_cand.addUserCand("trk2", trk2_ptr );
+	pipi_cand.addUserInt("trk1_idx", trk1_idx );
+	pipi_cand.addUserInt("trk2_idx", trk2_idx );
+
+	// minimal selection 
+	if( !pre_vtx_selection_(pipi_cand) ) continue;
+
+	// Saver further quantities to be saved in the final ntuples
+	pipi_cand.addUserInt("mumu_idx", ll_idx );  
+
+	// save further quantities, to be saved in the final ntuples: pions before fit
+	// Pions post fit are saved only after the very final B fit
+	pipi_cand.addUserFloat("pi1_pt",  trk1_ptr->pt()); 
+	pipi_cand.addUserFloat("pi1_eta", trk1_ptr->eta());
+	pipi_cand.addUserFloat("pi1_phi", trk1_ptr->phi());
+	pipi_cand.addUserFloat("pi2_pt",  trk2_ptr->pt());
+	pipi_cand.addUserFloat("pi2_eta", trk2_ptr->eta());
+	pipi_cand.addUserFloat("pi2_phi", trk2_ptr->phi());
+	pipi_cand.addUserFloat("pi1_vx",  trk1_ptr->vx()); 
+	pipi_cand.addUserFloat("pi1_vy",  trk1_ptr->vy()); 
+	pipi_cand.addUserFloat("pi1_vz",  trk1_ptr->vz()); 
+	pipi_cand.addUserFloat("pi2_vx",  trk2_ptr->vx()); 
+	pipi_cand.addUserFloat("pi2_vy",  trk2_ptr->vy()); 
+	pipi_cand.addUserFloat("pi2_vz",  trk2_ptr->vz()); 
+
+	// Put in the event
+	pipi_out->push_back(pipi_cand);
+
+      } // Loop 1st pi
+    } // Loop 2nd pi
+  } // Loop muons  
+
   evt.put(std::move(pipi_out), "SelectedDiPions");
-  evt.put(std::move(kinVtx_out), "SelectedDiPionKinVtxs");   
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
