efd7d8cc3c498c4921ac0e4cdf80b31b6775fa2d
crovelli <chiara.rovelli@cern.ch>
2022-03-23 09:58:43 +0100

    first commit slim version

--- src/classes.h
+++ src/classes.h
@@ -4,14 +4,16 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "TrackingTools/TransientTrack/interface/TransientTrack.h"
 #include "PhysicsTools/XNano/plugins/KinVtxFitter.h"
-#include <vector>
+#include "PhysicsTools/XNano/plugins/KinVtxFitterWithMassConstraint.h"
 
+#include <vector>
 
 namespace {
   struct dictionary {
       std::vector<reco::TransientTrack> ttv;
       edm::Wrapper<std::vector<reco::TransientTrack> > wttv; 
       edm::Wrapper<std::vector<KinVtxFitter> > wkv;
+      edm::Wrapper<std::vector<KinVtxFitterWithMassConstraint> > wkvm;
   };
 }
 
