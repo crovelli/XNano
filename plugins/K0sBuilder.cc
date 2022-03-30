1f996cc116a59a8f684062c4b5fa346f03324a85
crovelli <chiara.rovelli@cern.ch>
2022-03-30 17:36:50 +0200

    Move to usage of Vee candidate

--- plugins/K0sBuilder.cc
+++ plugins/K0sBuilder.cc
@@ -12,15 +12,27 @@
 #include <memory>
 #include <map>
 #include <string>
+#include "TLorentzVector.h"
+
 #include "CommonTools/Utils/interface/StringCutObjectSelector.h"
 #include "DataFormats/PatCandidates/interface/CompositeCandidate.h"
 #include "DataFormats/Math/interface/deltaR.h"
 #include "CommonTools/Statistics/interface/ChiSquaredProbability.h"
 #include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/KinematicParticleFactoryFromTransientTrack.h"
+#include "RecoVertex/KinematicFit/interface/KinematicParticleVertexFitter.h"
+#include "RecoVertex/KinematicFit/interface/KinematicParticleFitter.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/RefCountedKinematicParticle.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/MultiTrackKinematicConstraint.h"
+#include "RecoVertex/KinematicFit/interface/TwoTrackMassKinematicConstraint.h"
+#include "RecoVertex/KinematicFit/interface/KinematicConstrainedVertexFitter.h"
 #include "helper.h"
 #include <limits>
 #include <algorithm>
 #include "KinVtxFitter.h"
+#include "KinVtxFitterWithMassConstraint.h"
 
 class K0sBuilder : public edm::global::EDProducer<> {
 
@@ -29,16 +41,13 @@ public:
   typedef std::vector<reco::TransientTrack> TransientTrackCollection;
   
   explicit K0sBuilder(const edm::ParameterSet &cfg):
-    trk1_selection_{cfg.getParameter<std::string>("trk1Selection")},
-    trk2_selection_{cfg.getParameter<std::string>("trk2Selection")},
-    pre_vtx_selection_{cfg.getParameter<std::string>("preVtxSelection")},
+    
     post_vtx_selection_{cfg.getParameter<std::string>("postVtxSelection")},
-    pfcands_{consumes<pat::CompositeCandidateCollection>( cfg.getParameter<edm::InputTag>("pfcands") )},
-    ttracks_{consumes<TransientTrackCollection>( cfg.getParameter<edm::InputTag>("transientTracks") )} {
+    svs_{consumes<reco::VertexCompositePtrCandidateCollection>( cfg.getParameter<edm::InputTag>("svSrc") )} {
       
       //output
       produces<pat::CompositeCandidateCollection>("SelectedK0s");
-      produces<std::vector<KinVtxFitter> >("SelectedK0sKinVtxs");
+      produces<std::vector<KinVtxFitterWithMassConstraint> >("SelectedK0sKinVtxsWMC");
     }
 
   ~K0sBuilder() override {}
@@ -48,95 +57,106 @@ public:
   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions) {}
   
 private:
-  const StringCutObjectSelector<pat::CompositeCandidate> trk1_selection_;     
-  const StringCutObjectSelector<pat::CompositeCandidate> trk2_selection_;     
-  const StringCutObjectSelector<pat::CompositeCandidate> pre_vtx_selection_;  
   const StringCutObjectSelector<pat::CompositeCandidate> post_vtx_selection_; 
-  const edm::EDGetTokenT<pat::CompositeCandidateCollection> pfcands_; 
-  const edm::EDGetTokenT<TransientTrackCollection> ttracks_; 
+  const edm::EDGetTokenT<reco::VertexCompositePtrCandidateCollection> svs_;
 };
 
 
-void K0sBuilder::produce(edm::StreamID, edm::Event &evt, edm::EventSetup const &) const {
+void K0sBuilder::produce(edm::StreamID, edm::Event &evt, edm::EventSetup const &iSetup) const {
 
   // inputs  
-  edm::Handle<pat::CompositeCandidateCollection> pfcands;
-  evt.getByToken(pfcands_, pfcands);  
-  edm::Handle<TransientTrackCollection> ttracks;
-  evt.getByToken(ttracks_, ttracks);
+  edm::Handle<reco::VertexCompositePtrCandidateCollection> v0Coll;
+  evt.getByToken(svs_, v0Coll);
+  
+  edm::ESHandle<TransientTrackBuilder> theB;
+  iSetup.get<TransientTrackRecord>().get("TransientTrackBuilder", theB);
   
   // output
   std::unique_ptr<pat::CompositeCandidateCollection> k0s_out(new pat::CompositeCandidateCollection());
-  std::unique_ptr<std::vector<KinVtxFitter> > kinVtx_out( new std::vector<KinVtxFitter> );  
+  std::unique_ptr<std::vector<KinVtxFitterWithMassConstraint> > kinVtx_out( new std::vector<KinVtxFitterWithMassConstraint> );
 
   // main loop
-  for(size_t trk1_idx = 0; trk1_idx < pfcands->size(); ++trk1_idx ){
-    edm::Ptr<pat::CompositeCandidate> trk1_ptr( pfcands, trk1_idx );
-    if(!trk1_selection_(*trk1_ptr)) continue; 
+  for (reco::VertexCompositePtrCandidateCollection::const_iterator vee = v0Coll->begin(); vee != v0Coll->end(); vee++) {
     
-    for(size_t trk2_idx = trk1_idx + 1; trk2_idx < pfcands->size(); ++trk2_idx) {
-      edm::Ptr<pat::CompositeCandidate> trk2_ptr( pfcands, trk2_idx );
-      if(!trk2_selection_(*trk2_ptr)) continue;
-
-      // require opposite mass
-      if (trk1_ptr->charge() == trk2_ptr->charge()) continue; 
-      
-      // create a K0S candidate; 
-      pat::CompositeCandidate k0s_cand;
-      auto trk1_p4=trk1_ptr->polarP4();
-      auto trk2_p4=trk2_ptr->polarP4();
-      trk1_p4.SetM(PI_MASS);
-      trk2_p4.SetM(PI_MASS);
-
-      // adding stuff for pre fit selection
-      k0s_cand.setP4(trk1_p4 + trk2_p4);
-      k0s_cand.addUserFloat("trk_deltaR", reco::deltaR(*trk1_ptr, *trk2_ptr));
-
-      // save indices
-      k0s_cand.addUserInt("trk1_idx", trk1_idx );
-      k0s_cand.addUserInt("trk2_idx", trk2_idx );
-
-      // save cands      
-      k0s_cand.addUserCand("trk1", trk1_ptr );
-      k0s_cand.addUserCand("trk2", trk2_ptr );
-
-      // selection before fit
-      if( !pre_vtx_selection_(k0s_cand) ) continue;
-           
-      KinVtxFitter fitter(
-			  {ttracks->at(trk1_idx), ttracks->at(trk2_idx)},
-			  {PI_MASS, PI_MASS },
-			  {PI_SIGMA, PI_SIGMA} 
-			  );
-      if ( !fitter.success() ) continue;           
-
-      // save quantities after fit
-      k0s_cand.addUserFloat("sv_chi2", fitter.chi2());
-      k0s_cand.addUserFloat("sv_ndof", fitter.dof()); 
-      k0s_cand.addUserFloat("sv_prob", fitter.prob());    
-      k0s_cand.addUserFloat("fitted_mass", fitter.fitted_candidate().mass() );
-      k0s_cand.addUserFloat("fitted_pt", fitter.fitted_candidate().globalMomentum().perp() );
-      k0s_cand.addUserFloat("fitted_eta", fitter.fitted_candidate().globalMomentum().eta() );
-      k0s_cand.addUserFloat("fitted_phi", fitter.fitted_candidate().globalMomentum().phi() );
-
-      // refitted daughters (tracks)     
-      // std::vector<std::string> dnames{ "trk1", "trk2" };
-      // for (size_t idaughter=0; idaughter<dnames.size(); idaughter++){
-      // k0s_cand.addUserFloat("fitted_" + dnames[idaughter] + "_pt" ,fitter.daughter_p4(idaughter).pt() );
-      // k0s_cand.addUserFloat("fitted_" + dnames[idaughter] + "_eta",fitter.daughter_p4(idaughter).eta() );
-      // k0s_cand.addUserFloat("fitted_" + dnames[idaughter] + "_phi",fitter.daughter_p4(idaughter).phi() );
-      // }
-      
-      // after fit selection
-      if( !post_vtx_selection_(k0s_cand) ) continue;
-
-      k0s_out->push_back(k0s_cand);
-      kinVtx_out->push_back(fitter); 
+    // Order pions in decreasing pT
+    reco::TransientTrack pi1TT; 
+    reco::TransientTrack pi2TT; 
+    if (vee->daughter(0)->momentum().mag2() < vee->daughter(1)->momentum().mag2()) {
+      pi1TT = theB->build( vee->daughter(0)->bestTrack() );  
+      pi2TT = theB->build( vee->daughter(1)->bestTrack() );
+    } else {
+      pi1TT = theB->build( vee->daughter(1)->bestTrack() );
+      pi2TT = theB->build( vee->daughter(0)->bestTrack() );
     }
-  }
+    if(!pi1TT.isValid()) continue;
+    if(!pi2TT.isValid()) continue;
+    
+    // K0s Candidate
+    pat::CompositeCandidate k0s_cand;
+    k0s_cand.addUserFloat("prefit_mass", vee->mass()); 
+
+    // Vertex fit without mass constraint
+    KinVtxFitter fitterNMC(
+			   {pi1TT, pi2TT},
+			   {PI_MASS, PI_MASS },
+			   {PI_SIGMA, PI_SIGMA} 
+			   );
+    if ( !fitterNMC.success() ) continue;      
+    k0s_cand.addUserFloat("fitted_mass_womc", fitterNMC.fitted_candidate().mass());   
+
+    // Vertex fit refinement, adding mass constraint
+    KinVtxFitterWithMassConstraint fitter(KSHORT_MASS, KSHORT_SIGMA, fitterNMC.vtx_tree());
+    if ( !fitter.success() ) continue;   
+    
+    // Save quantities after fit
+    TLorentzVector p4k0s;
+    p4k0s.SetPtEtaPhiM(fitter.fitted_candidate().globalMomentum().perp(), 
+		       fitter.fitted_candidate().globalMomentum().eta(),
+		       fitter.fitted_candidate().globalMomentum().phi(),
+		       KSHORT_MASS);
+    RefCountedKinematicVertex fitted_vtx = fitter.fitted_refvtx();
+
+    // Selection after fit 
+    k0s_cand.addUserFloat("sv_prob", fitter.prob());    
+    k0s_cand.addUserFloat("fitted_mass", fitter.fitted_candidate().mass() );
+    if( !post_vtx_selection_(k0s_cand) ) continue;
+
+
+    // For selected K0s add more infos
+    k0s_cand.setP4(vee->polarP4());
+    if (vee->daughter(0)->momentum().mag2() < vee->daughter(1)->momentum().mag2()) {
+      k0s_cand.addUserFloat("trk1_pt",   vee->daughterPtr(0)->bestTrack()->pt());
+      k0s_cand.addUserFloat("trk1_eta",  vee->daughterPtr(0)->bestTrack()->eta());
+      k0s_cand.addUserFloat("trk1_phi",  vee->daughterPtr(0)->bestTrack()->phi());
+      k0s_cand.addUserInt("trk1_charge", vee->daughterPtr(0)->bestTrack()->charge()); 
+      k0s_cand.addUserFloat("trk2_pt",   vee->daughterPtr(1)->bestTrack()->pt());
+      k0s_cand.addUserFloat("trk2_eta",  vee->daughterPtr(1)->bestTrack()->eta());
+      k0s_cand.addUserFloat("trk2_phi",  vee->daughterPtr(1)->bestTrack()->phi());
+      k0s_cand.addUserInt("trk2_charge", vee->daughterPtr(1)->bestTrack()->charge()); 
+    } else {
+      k0s_cand.addUserFloat("trk1_pt",   vee->daughterPtr(1)->bestTrack()->pt());
+      k0s_cand.addUserFloat("trk1_eta",  vee->daughterPtr(1)->bestTrack()->eta());
+      k0s_cand.addUserFloat("trk1_phi",  vee->daughterPtr(1)->bestTrack()->phi());
+      k0s_cand.addUserInt("trk1_charge", vee->daughterPtr(1)->bestTrack()->charge()); 
+      k0s_cand.addUserFloat("trk2_pt",   vee->daughterPtr(0)->bestTrack()->pt());
+      k0s_cand.addUserFloat("trk2_eta",  vee->daughterPtr(0)->bestTrack()->eta());
+      k0s_cand.addUserFloat("trk2_phi",  vee->daughterPtr(0)->bestTrack()->phi());
+      k0s_cand.addUserInt("trk2_charge", vee->daughterPtr(0)->bestTrack()->charge()); 
+    }
+    
+    // save quantities after fit
+    k0s_cand.addUserFloat("fitted_pt", fitter.fitted_candidate().globalMomentum().perp() );
+    k0s_cand.addUserFloat("fitted_eta", fitter.fitted_candidate().globalMomentum().eta() );
+    k0s_cand.addUserFloat("fitted_phi", fitter.fitted_candidate().globalMomentum().phi() );
+    
+    // Put in the event
+    k0s_out->push_back(k0s_cand);  
+    kinVtx_out->push_back(fitter);
+
+  } // Loop over V0s
   
   evt.put(std::move(k0s_out), "SelectedK0s");
-  evt.put(std::move(kinVtx_out), "SelectedK0sKinVtxs");   
+  evt.put(std::move(kinVtx_out), "SelectedK0sKinVtxsWMC");
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
