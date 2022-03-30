75435bd1e828696813abb5a79fddceb3dbe1e0cc
crovelli <chiara.rovelli@cern.ch>
2022-03-30 15:46:50 +0200

    Fit at vertex removed - not used

--- plugins/PiPiBuilder.cc
+++ plugins/PiPiBuilder.cc
@@ -32,11 +32,9 @@ public:
     trk1_selection_{cfg.getParameter<std::string>("trk1Selection")},
     trk2_selection_{cfg.getParameter<std::string>("trk2Selection")}, 
     pre_vtx_selection_{cfg.getParameter<std::string>("preVtxSelection")},
-    post_vtx_selection_{cfg.getParameter<std::string>("postVtxSelection")},
     pfcands_{consumes<pat::CompositeCandidateCollection>( cfg.getParameter<edm::InputTag>("pfcands") )},
     ttracks_{consumes<TransientTrackCollection>( cfg.getParameter<edm::InputTag>("transientTracks") )} {
       produces<pat::CompositeCandidateCollection>("SelectedDiPions");
-      produces<std::vector<KinVtxFitter> >("SelectedDiPionKinVtxs");  
     }
 
   ~PiPiBuilder() override {}
@@ -49,7 +47,6 @@ private:
   const StringCutObjectSelector<pat::CompositeCandidate> trk1_selection_;     
   const StringCutObjectSelector<pat::CompositeCandidate> trk2_selection_;     
   const StringCutObjectSelector<pat::CompositeCandidate> pre_vtx_selection_;
-  const StringCutObjectSelector<pat::CompositeCandidate> post_vtx_selection_;
   const edm::EDGetTokenT<pat::CompositeCandidateCollection> pfcands_; 
   const edm::EDGetTokenT<TransientTrackCollection> ttracks_; 
 };
@@ -65,7 +62,6 @@ void PiPiBuilder::produce(edm::StreamID, edm::Event &evt, edm::EventSetup const 
   
   // output
   std::unique_ptr<pat::CompositeCandidateCollection> pipi_out(new pat::CompositeCandidateCollection());
-  std::unique_ptr<std::vector<KinVtxFitter> > kinVtx_out( new std::vector<KinVtxFitter> );   
   
   // main loop
   for(size_t trk1_idx = 0; trk1_idx < pfcands->size(); ++trk1_idx ){
@@ -98,40 +94,14 @@ void PiPiBuilder::produce(edm::StreamID, edm::Event &evt, edm::EventSetup const 
       pipi_cand.addUserInt("trk1_idx", trk1_idx );
       pipi_cand.addUserInt("trk2_idx", trk2_idx );
       
-      // selection before fit
+      // minimal selection 
       if( !pre_vtx_selection_(pipi_cand) ) continue;
 
-      KinVtxFitter fitter(
-			  {ttracks->at(trk1_idx), ttracks->at(trk2_idx)},
-			  {PI_MASS, PI_MASS },
-			  {PI_SIGMA, PI_SIGMA} 
-			  );
-      if ( !fitter.success() ) continue;           
-      
-      // save quantities after fit
-      pipi_cand.addUserFloat("sv_chi2", fitter.chi2());
-      pipi_cand.addUserFloat("sv_ndof", fitter.dof()); 
-      pipi_cand.addUserFloat("sv_prob", fitter.prob());    
-      pipi_cand.addUserFloat("fitted_mass", fitter.fitted_candidate().mass());
-
-      // refitted daughters (tracks)     
-      // std::vector<std::string> dnames{ "trk1", "trk2" };
-      // for (size_t idaughter=0; idaughter<dnames.size(); idaughter++){
-      // pipi_cand.addUserFloat("fitted_" + dnames[idaughter] + "_pt" ,fitter.daughter_p4(idaughter).pt() );
-      // pipi_cand.addUserFloat("fitted_" + dnames[idaughter] + "_eta",fitter.daughter_p4(idaughter).eta() );
-      // pipi_cand.addUserFloat("fitted_" + dnames[idaughter] + "_phi",fitter.daughter_p4(idaughter).phi() );
-      // }
-
-      // after fit selection 
-      if( !post_vtx_selection_(pipi_cand) ) continue;
-
       pipi_out->push_back(pipi_cand);
-      kinVtx_out->push_back(fitter);   
     }
   }
   
   evt.put(std::move(pipi_out), "SelectedDiPions");
-  evt.put(std::move(kinVtx_out), "SelectedDiPionKinVtxs");   
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
