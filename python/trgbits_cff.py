ae38572798ef34a1ea39eafd9ef77e388ccfeee2
crovelli <chiara.rovelli@cern.ch>
2022-04-28 11:32:50 +0200

    only selected HLT paths

--- python/trgbits_cff.py
+++ python/trgbits_cff.py
@@ -7,20 +7,10 @@ trgTable = cms.EDProducer( "TrgBitTableProducer",
                           # interesting paths for 2017 and 2018
                           paths      = cms.vstring(
                                              "HLT_Dimuon25_Jpsi",                        # inclusive dimuon jpsi
-                                             "HLT_Dimuon20_Jpsi_Barrel_Seagulls",        # inclusive dimuon jpsi
                                              "HLT_DoubleMu4_JpsiTrk_Displaced",          # displaced jpistrk or jpsi trktrk
                                              "HLT_DoubleMu4_JpsiTrkTrk_Displaced",       # displaced jpistrk or jpsi trktrk
-                                             "HLT_DoubleMu4_3_Jpsi_Displaced",           # prescaled for 2017
-                                             "HLT_DoubleMu4_3_Jpsi",                     # prescaled for 2018  
-                                             "HLT_DoubleMu4_Jpsi_Displaced",             # prescaled
                                              "HLT_Dimuon18_PsiPrime",                    # inclusive dimuon psi2s
-                                             "HLT_Dimuon10_PsiPrime_Barrel_Seagulls",    # inclusive dimuon psi2s
-                                             "HLT_DoubleMu4_PsiPrimeTrk_Displaced",      # displaced psi2s trk 
-                                             "HLT_Dimuon0_Jpsi3p5_Muon2",                # triple-mu (jpsi + muon)
-                                             "HLT_DoubleMu2_Jpsi_DoubleTkMu0_Phi",       # jpsi + 2 trkmu (phi->mumu)
-                                             "HLT_DoubleMu2_Jpsi_DoubleTrk1_Phi",        # jpsi+2 trk(phi->KK) for 2017 
-                                             "HLT_DoubleMu2_Jpsi_DoubleTrk1_Phi1p05",    # jpsi+2 trk(phi->KK) for 2018
-                                             "HLT_DoubleMu4_3_Bs"                        # Livia 
+                                             "HLT_DoubleMu4_PsiPrimeTrk_Displaced"       # displaced psi2s trk 
                                               ),
 
                           # interesting paths for 2016 
