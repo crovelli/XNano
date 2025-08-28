6674245503d3f8827cb8e0aa6ef7e45a02514b51
crovelli <chiara.rovelli@cern.ch>
2025-08-28 14:15:41 +0200

    porting

--- test/run_nano_2022_cfg.py
+++ test/run_nano_2022_cfg.py
@@ -8,6 +8,16 @@ options.register('isMC', True,
     VarParsing.varType.bool,
     "Run this on real data"
 )
+options.register('Era','2022postEE',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "Set data taking period : 2022preEE, 2022postEE, 2023preBPix, 2023postBPix"
+)
+options.register('physProcess','X3872',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "Set the phys process : X3872"
+)
 options.register('globalTag', 'NOTSET',
     VarParsing.multiplicity.singleton,
     VarParsing.varType.string,
@@ -34,52 +44,80 @@ options.register('skip', 0,
     "skip first N events"
 )
 
-options.setDefault('maxEvents', -1)
-#options.setDefault('maxEvents', 1000)
-options.setDefault('tag', '10215')
 options.parseArguments()
 
-# global tags:
-# MC pre ECAL leakage:  124X_mcRun3_2022_realistic_v12
-# MC post ECAL leakage: 124X_mcRun3_2022_realistic_postEE_v1
-globaltag = '124X_dataRun3_v14' if not options.isMC else '124X_mcRun3_2022_realistic_v12' 
+era = options.Era
+phys_process = options.physProcess
+if not options.tag :
+    tag = '_'.join([phys_process, era]) 
+    options.setDefault('tag', tag)
 
+# set number of events
+options.setDefault('maxEvents', 100)
+
+# set global tag:
+global_tags_mc = {
+    '2022preEE'     : '130X_mcRun3_2022_realistic_v5',
+    '2022postEE'    : '130X_mcRun3_2022_realistic_postEE_v6',
+    '2023preBPix'   : '130X_mcRun3_2023_realistic_v14',
+    '2023postBPix'  : '130X_mcRun3_2023_realistic_postBPix_v2',
+}
+global_tags_data = {
+    '2022preEE'     : '124X_dataRun3_PromptAnalysis_v1', #era CD use '124X_dataRun3_Prompt_v10' for era E
+    '2022postEE'    : '130X_dataRun3_PromptAnalysis_v1', #era FG
+    '2023preBPix'   : '130X_dataRun3_PromptAnalysis_v1', #era BC
+    '2023postBPix'  : '130X_dataRun3_PromptAnalysis_v1', #era D
+}
+ 
 if options._beenSet['globalTag']:
     globaltag = options.globalTag
+else:
+    globaltag = global_tags_mc[era] if options.isMC else global_tags_data[era] 
 
 extension = {False : 'data', True : 'mc'}
 outputFileNANO = cms.untracked.string('_'.join(['xNANO', extension[options.isMC], options.tag])+'.root')
 outputFileFEVT = cms.untracked.string('_'.join(['xFullEvt', extension[options.isMC], options.tag])+'.root')
-if not options.inputFiles:
-    options.inputFiles = ['/store/data/Run2022D/ParkingDoubleMuonLowMass0/MINIAOD/PromptReco-v2/000/357/734/00000/0f35818c-326c-4d85-a190-3bdec4664b2c.root'] if not options.isMC else \
-                         ['/store/user/soffi/BPH_BToPsi2SKs_Psi2SToJPsiPiPi_JPsiToMuMu_McM_Run3/Run3Summer22_MiniAODv3/230215_082537/0000/MiniAODv3_1.root',
-                          '/store/user/soffi/BPH_BToPsi2SKs_Psi2SToJPsiPiPi_JPsiToMuMu_McM_Run3/Run3Summer22_MiniAODv3/230215_082537/0000/MiniAODv3_2.root',
-                          '/store/user/soffi/BPH_BToPsi2SKs_Psi2SToJPsiPiPi_JPsiToMuMu_McM_Run3/Run3Summer22_MiniAODv3/230215_082537/0000/MiniAODv3_3.root',
-                          '/store/user/soffi/BPH_BToPsi2SKs_Psi2SToJPsiPiPi_JPsiToMuMu_McM_Run3/Run3Summer22_MiniAODv3/230215_082537/0000/MiniAODv3_4.root',
-                          '/store/user/soffi/BPH_BToPsi2SKs_Psi2SToJPsiPiPi_JPsiToMuMu_McM_Run3/Run3Summer22_MiniAODv3/230215_082537/0000/MiniAODv3_5.root',
-                          '/store/user/soffi/BPH_BToPsi2SKs_Psi2SToJPsiPiPi_JPsiToMuMu_McM_Run3/Run3Summer22_MiniAODv3/230215_082537/0000/MiniAODv3_6.root',
-                          '/store/user/soffi/BPH_BToPsi2SKs_Psi2SToJPsiPiPi_JPsiToMuMu_McM_Run3/Run3Summer22_MiniAODv3/230215_082537/0000/MiniAODv3_7.root',
-                          '/store/user/soffi/BPH_BToPsi2SKs_Psi2SToJPsiPiPi_JPsiToMuMu_McM_Run3/Run3Summer22_MiniAODv3/230215_082537/0000/MiniAODv3_8.root',
-                          '/store/user/soffi/BPH_BToPsi2SKs_Psi2SToJPsiPiPi_JPsiToMuMu_McM_Run3/Run3Summer22_MiniAODv3/230215_082537/0000/MiniAODv3_9.root',
-                          '/store/user/soffi/BPH_BToPsi2SKs_Psi2SToJPsiPiPi_JPsiToMuMu_McM_Run3/Run3Summer22_MiniAODv3/230215_082537/0000/MiniAODv3_10.root'
-                         ]
+
+# test input files per process and era
+test_mc_inFiles_process_era = {
+     'X3872' : {
+         '2022preEE'     : ['/store/mc/Run3Summer22MiniAODv4/BuToX3872Ks_X3872ToJPsiRho_JPsiToMuMu_RhoToPiPi_TuneCP5_13TeV_pythia8-evtgen/MINIAODSIM/130X_mcRun3_2022_realistic_v5-v2/2520000/3db65477-4aea-4dd6-a194-fa111395ba8e.root'],   # NB: si chiama 13 TeV ma sembra essere 13.6, https://cms-pdmv-prod.web.cern.ch/mcm/requests?prepid=BPH-Run3Summer22GS-00020&shown=262271
+        '2022postEE'    : ['/store/mc/Run3Summer22EEMiniAODv4/BuToX3872Ks_X3872ToJPsiRho_JPsiToMuMu_RhoToPiPi_TuneCP5_13p6TeV_pythia8-evtgen/MINIAODSIM/130X_mcRun3_2022_realistic_postEE_v6-v2/60000/02bdd816-3a70-4b3d-bdf9-021756af1a04.root'],
+        '2023preBPix'   : [''],
+        '2023postBPix'  : [''], 
+    },
+}
+
+test_data_inFiles_era = {
+    '2022preEE'     : [], #era CDE
+    '2022postEE'    : ['/store/data/Run2022F/ParkingDoubleMuonLowMass0/MINIAOD/22Sep2023-v1/60000/00807cd1-61e0-4a30-917a-f2957e4365b0.root',
+                        '/store/data/Run2022F/ParkingDoubleMuonLowMass0/MINIAOD/22Sep2023-v1/60000/0163f2af-82a7-4540-8702-b111818a93d4.root'
+                    ], #era FG
+    '2023preBPix'   : [], #era BC
+    '2023postBPix'  : [], #era D
+}
+
+# Define inputs
+if not options.inputFiles :
+    options.inputFiles = test_mc_inFiles_process_era[phys_process][era] if options.isMC else test_data_inFiles_era[era]
 annotation = '%s nevts:%d' % (outputFileNANO, options.maxEvents)
 
 #from Configuration.StandardSequences.Eras import eras
-process = cms.Process('BParkNANO')
+process = cms.Process('XNANO')
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
+#process.load('Configuration.Geometry.GeometryIdeal_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load('PhysicsTools.XNano.nanoX_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
-#process.MessageLogger.cerr.FwkReport.reportEvery = options.reportEvery
+process.MessageLogger.cerr.FwkReport.reportEvery = options.reportEvery
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(options.maxEvents)
 )
